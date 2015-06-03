a373db5dd34e62b7510b0de809d4f70ef23fd9c0
NKILI OBELE KAREN FIFI <knkiliob@BE19-01.ens-sfa.univ-poitiers.fr>
2015-06-03 16:09:42 +0200

    Correction de la base de données

--- SQL/projet_stage.sql
+++ SQL/projet_stage.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 26 Mai 2015 à 09:55
+-- Généré le :  Mer 03 Juin 2015 à 16:07
 -- Version du serveur :  5.5.41-0+wheezy1
 -- Version de PHP :  5.4.36-0+deb7u3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `jbrach02`
+-- Base de données :  `knkiliob`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `sta_annees` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `semestres_id` int(10) NOT NULL,
+  `semestre_id` int(10) NOT NULL,
   `annee1` year(4) NOT NULL,
   `annee2` year(4) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `semestres_id` (`semestres_id`)
+  KEY `semestres_id` (`semestre_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -46,11 +46,11 @@ CREATE TABLE IF NOT EXISTS `sta_annees_etudiants` (
   `moyAn` float NOT NULL,
   `admis` tinyint(1) NOT NULL,
   `compensable` tinyint(1) NOT NULL,
-  `etudiants_id` int(10) NOT NULL,
-  `annees_id` int(10) NOT NULL,
+  `etudiant_id` int(10) NOT NULL,
+  `annee_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `etudiants_id` (`etudiants_id`),
-  KEY `annees_id` (`annees_id`)
+  KEY `etudiants_id` (`etudiant_id`),
+  KEY `annees_id` (`annee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -61,8 +61,8 @@ CREATE TABLE IF NOT EXISTS `sta_annees_etudiants` (
 
 CREATE TABLE IF NOT EXISTS `sta_diplomes` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `diplome` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
-  `description` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `diplome` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -73,8 +73,8 @@ CREATE TABLE IF NOT EXISTS `sta_diplomes` (
 --
 
 CREATE TABLE IF NOT EXISTS `sta_enseignants` (
-  `users_id` int(10) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`users_id`)
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -85,11 +85,11 @@ CREATE TABLE IF NOT EXISTS `sta_enseignants` (
 
 CREATE TABLE IF NOT EXISTS `sta_enseignants_ues` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `enseignants_id` int(10) NOT NULL,
-  `ues_id` int(10) NOT NULL,
+  `enseignant_id` int(10) NOT NULL,
+  `ue_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `enseignants_id` (`enseignants_id`),
-  KEY `ues_id` (`ues_id`)
+  KEY `enseignants_id` (`enseignant_id`),
+  KEY `ues_id` (`ue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -99,9 +99,9 @@ CREATE TABLE IF NOT EXISTS `sta_enseignants_ues` (
 --
 
 CREATE TABLE IF NOT EXISTS `sta_etudiants` (
-  `users_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
   `numero` varchar(50) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`users_id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -115,11 +115,11 @@ CREATE TABLE IF NOT EXISTS `sta_etudiants_semestres` (
   `moySem` float NOT NULL,
   `valide` tinyint(1) NOT NULL,
   `compensable` tinyint(1) NOT NULL,
-  `etudiants_id` int(10) NOT NULL,
-  `semestres_id` int(10) NOT NULL,
+  `etudiant_id` int(10) NOT NULL,
+  `semestre_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `etudiants_id` (`etudiants_id`),
-  KEY `semestres_id` (`semestres_id`)
+  KEY `etudiants_id` (`etudiant_id`),
+  KEY `semestres_id` (`semestre_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -133,14 +133,14 @@ CREATE TABLE IF NOT EXISTS `sta_etudiants_types_notes_ues` (
   `absence` tinyint(1) NOT NULL,
   `session1` tinyint(1) NOT NULL,
   `note` float DEFAULT NULL,
-  `ues_id` int(10) NOT NULL,
-  `types_notes_id` int(10) NOT NULL,
-  `etudiants_id` int(10) NOT NULL,
-  `typeAbsence` varchar(5) CHARACTER SET latin1 DEFAULT NULL,
+  `ue_id` int(10) NOT NULL,
+  `types_note_id` int(10) NOT NULL,
+  `etudiant_id` int(10) NOT NULL,
+  `typeAbsence` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `ues_id` (`ues_id`),
-  KEY `types_notes_id` (`types_notes_id`),
-  KEY `etudiants_id` (`etudiants_id`)
+  KEY `ues_id` (`ue_id`),
+  KEY `types_notes_id` (`types_note_id`),
+  KEY `etudiants_id` (`etudiant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -152,11 +152,11 @@ CREATE TABLE IF NOT EXISTS `sta_etudiants_types_notes_ues` (
 CREATE TABLE IF NOT EXISTS `sta_etudiants_ues` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `dispense` tinyint(1) NOT NULL,
-  `ues_id` int(10) NOT NULL,
-  `etudiants_id` int(10) NOT NULL,
+  `ue_id` int(10) NOT NULL,
+  `etudiant_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `ues_id` (`ues_id`),
-  KEY `etudiants_id` (`etudiants_id`)
+  KEY `ues_id` (`ue_id`),
+  KEY `etudiants_id` (`etudiant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -167,11 +167,11 @@ CREATE TABLE IF NOT EXISTS `sta_etudiants_ues` (
 
 CREATE TABLE IF NOT EXISTS `sta_responsables` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `enseignants_id` int(10) NOT NULL,
-  `ues_id` int(10) NOT NULL,
+  `enseignant_id` int(10) NOT NULL,
+  `ue_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `enseignants_id` (`enseignants_id`),
-  KEY `ues_id` (`ues_id`)
+  KEY `enseignants_id` (`enseignant_id`),
+  KEY `ues_id` (`ue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -183,10 +183,10 @@ CREATE TABLE IF NOT EXISTS `sta_responsables` (
 CREATE TABLE IF NOT EXISTS `sta_semestres` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `numSem` int(10) NOT NULL,
-  `diplomes_id` int(10) NOT NULL,
+  `diplome_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `annees_id` (`diplomes_id`),
-  KEY `diplomes_id` (`diplomes_id`)
+  KEY `annees_id` (`diplome_id`),
+  KEY `diplomes_id` (`diplome_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -197,8 +197,8 @@ CREATE TABLE IF NOT EXISTS `sta_semestres` (
 
 CREATE TABLE IF NOT EXISTS `sta_types_notes` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) CHARACTER SET latin1 NOT NULL,
-  `sigle` varchar(5) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `sigle` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -210,13 +210,13 @@ CREATE TABLE IF NOT EXISTS `sta_types_notes` (
 
 CREATE TABLE IF NOT EXISTS `sta_types_notes_ues` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `types_notes_id` int(10) NOT NULL,
-  `ues_id` int(10) NOT NULL,
+  `types_note_id` int(10) NOT NULL,
+  `ue_id` int(10) NOT NULL,
   `coefNo` int(10) NOT NULL,
   `session1` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `types_notes_id` (`types_notes_id`),
-  KEY `ues_id` (`ues_id`)
+  KEY `types_notes_id` (`types_note_id`),
+  KEY `ues_id` (`ue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -227,7 +227,7 @@ CREATE TABLE IF NOT EXISTS `sta_types_notes_ues` (
 
 CREATE TABLE IF NOT EXISTS `sta_ues` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(100) NOT NULL,
   `sup1` tinyint(1) NOT NULL,
   `sup2` tinyint(1) NOT NULL,
   `obligatoire` tinyint(1) NOT NULL,
@@ -245,11 +245,11 @@ CREATE TABLE IF NOT EXISTS `sta_ues` (
 
 CREATE TABLE IF NOT EXISTS `sta_users` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `lastname` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `username` varchar(32) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(32) CHARACTER SET latin1 NOT NULL,
-  `role` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `role` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -261,75 +261,75 @@ CREATE TABLE IF NOT EXISTS `sta_users` (
 -- Contraintes pour la table `sta_annees`
 --
 ALTER TABLE `sta_annees`
-  ADD CONSTRAINT `sta_annees_ibfk_1` FOREIGN KEY (`semestres_id`) REFERENCES `sta_semestres` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_annees_ibfk_1` FOREIGN KEY (`semestre_id`) REFERENCES `sta_semestres` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_annees_etudiants`
 --
 ALTER TABLE `sta_annees_etudiants`
-  ADD CONSTRAINT `sta_annees_etudiants_ibfk_2` FOREIGN KEY (`annees_id`) REFERENCES `sta_annees` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_annees_etudiants_ibfk_1` FOREIGN KEY (`etudiants_id`) REFERENCES `sta_etudiants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_annees_etudiants_ibfk_3` FOREIGN KEY (`etudiant_id`) REFERENCES `sta_etudiants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_annees_etudiants_ibfk_2` FOREIGN KEY (`annee_id`) REFERENCES `sta_annees` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_enseignants`
 --
 ALTER TABLE `sta_enseignants`
-  ADD CONSTRAINT `sta_enseignants_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `sta_users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_enseignants_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `sta_users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_enseignants_ues`
 --
 ALTER TABLE `sta_enseignants_ues`
-  ADD CONSTRAINT `sta_enseignants_ues_ibfk_2` FOREIGN KEY (`ues_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_enseignants_ues_ibfk_1` FOREIGN KEY (`enseignants_id`) REFERENCES `sta_enseignants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_enseignants_ues_ibfk_2` FOREIGN KEY (`ue_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_enseignants_ues_ibfk_1` FOREIGN KEY (`enseignant_id`) REFERENCES `sta_enseignants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_etudiants`
 --
 ALTER TABLE `sta_etudiants`
-  ADD CONSTRAINT `sta_etudiants_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `sta_users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_etudiants_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `sta_users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_etudiants_semestres`
 --
 ALTER TABLE `sta_etudiants_semestres`
-  ADD CONSTRAINT `sta_etudiants_semestres_ibfk_2` FOREIGN KEY (`semestres_id`) REFERENCES `sta_semestres` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_etudiants_semestres_ibfk_1` FOREIGN KEY (`etudiants_id`) REFERENCES `sta_etudiants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_etudiants_semestres_ibfk_2` FOREIGN KEY (`semestre_id`) REFERENCES `sta_semestres` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_etudiants_semestres_ibfk_1` FOREIGN KEY (`etudiant_id`) REFERENCES `sta_etudiants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_etudiants_types_notes_ues`
 --
 ALTER TABLE `sta_etudiants_types_notes_ues`
-  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_3` FOREIGN KEY (`etudiants_id`) REFERENCES `sta_etudiants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_1` FOREIGN KEY (`ues_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_2` FOREIGN KEY (`types_notes_id`) REFERENCES `sta_types_notes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_3` FOREIGN KEY (`etudiant_id`) REFERENCES `sta_etudiants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_1` FOREIGN KEY (`ue_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_etudiants_types_notes_ues_ibfk_2` FOREIGN KEY (`types_note_id`) REFERENCES `sta_types_notes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_etudiants_ues`
 --
 ALTER TABLE `sta_etudiants_ues`
-  ADD CONSTRAINT `sta_etudiants_ues_ibfk_2` FOREIGN KEY (`etudiants_id`) REFERENCES `sta_etudiants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_etudiants_ues_ibfk_1` FOREIGN KEY (`ues_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_etudiants_ues_ibfk_2` FOREIGN KEY (`etudiant_id`) REFERENCES `sta_etudiants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_etudiants_ues_ibfk_1` FOREIGN KEY (`ue_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_responsables`
 --
 ALTER TABLE `sta_responsables`
-  ADD CONSTRAINT `sta_responsables_ibfk_2` FOREIGN KEY (`ues_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_responsables_ibfk_1` FOREIGN KEY (`enseignants_id`) REFERENCES `sta_enseignants` (`users_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_responsables_ibfk_2` FOREIGN KEY (`ue_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_responsables_ibfk_1` FOREIGN KEY (`enseignant_id`) REFERENCES `sta_enseignants` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_semestres`
 --
 ALTER TABLE `sta_semestres`
-  ADD CONSTRAINT `sta_semestres_ibfk_1` FOREIGN KEY (`diplomes_id`) REFERENCES `sta_diplomes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_semestres_ibfk_1` FOREIGN KEY (`diplome_id`) REFERENCES `sta_diplomes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_types_notes_ues`
 --
 ALTER TABLE `sta_types_notes_ues`
-  ADD CONSTRAINT `sta_types_notes_ues_ibfk_2` FOREIGN KEY (`ues_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `sta_types_notes_ues_ibfk_1` FOREIGN KEY (`types_notes_id`) REFERENCES `sta_types_notes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `sta_types_notes_ues_ibfk_2` FOREIGN KEY (`ue_id`) REFERENCES `sta_ues` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `sta_types_notes_ues_ibfk_1` FOREIGN KEY (`types_note_id`) REFERENCES `sta_types_notes` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `sta_ues`
