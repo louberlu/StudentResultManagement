6d642a8e44f8e4f516c32a0f2f6970bc866d2bb1
NKILI OBELE KAREN FIFI <knkiliob@BE19-01.ens-sfa.univ-poitiers.fr>
2015-06-15 17:04:07 +0200

    Mise à jour du Controller etudiants

--- stage_cake/app/Controller/EtudiantsController.php
+++ stage_cake/app/Controller/EtudiantsController.php
@@ -1,4 +1,35 @@
 <?php
+/**
+ * Controller du model Etudiant
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Karen Fifi NKILI OBELE
+ * @author        Jonathan Brachet
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Model
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version       1.0.0
+ */
+ 
+ /**
+ * Classe du Controller EtudiantsController
+ *
+ *
+ * @author        Karen Fifi NKILI OBELE
+ * @author        Jonathan Brachet
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Model
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version       1.0.0
+ */
 class EtudiantsController extends AppController {
 
 	public function add()
@@ -12,9 +43,14 @@ class EtudiantsController extends AppController {
 			}
 			else
 			{
-				$this->Session->setFlash('Erreur dans l\'ajout de l\étudiant.');
+				$this->Session->setFlash('Erreur dans l\'ajout de l\'étudiant.');
 			}	
 		}
+	}
+	
+	public function edit()
+	{
+	   
 	}
 	
 	public function liste()
